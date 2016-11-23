94d4690568e4b56b3ced4ca18755a21fec0dc869
Tynan <quixotichamster@gmail.com>
2016-11-22 20:31:42 -0600

    Initializing the Spring files

---
+++ src/main/java/io/planit/PlanItApplication.java
@@ -0,0 +1,12 @@
+package io.planit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanItApplication.class, args);
+	}
+}
